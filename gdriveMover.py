0fe5ba94cd932002e55d0335cec8c27edd74093d
yuval <yshechter@umass.edu>
2020-04-20 15:48:38 -0400

    fixed buggy move script

--- gdriveMover.py
+++ gdriveMover.py
@@ -4,14 +4,15 @@ import time
 import shutil
 
 def gdriveMove(folderpath, mountpath, movepath):
-    if(os.path.ismount(mountpath)):
         for folder in os.listdir(folderpath):
-            shutil.move(folderpath+folder, mountpath+movepath+folder)
-    else:
-        subprocess.run(["fusermount","-u",mountpath])
-        time.sleep(5)
-        subprocess.run(["google-drive-ocamlfuse",mountpath])
-        time.sleep(5)
+            if(os.path.ismount(mountpath)):
+                shutil.move(folderpath+folder, mountpath+movepath+folder)
+            else:
+                subprocess.run(["fusermount","-u",mountpath])
+                time.sleep(5)
+                subprocess.run(["google-drive-ocamlfuse",mountpath])
+                time.sleep(5)
+                shutil.move(folderpath+folder, mountpath+movepath+folder)
 
 if __name__ == "__main__":
         gdriveMove("All Songs/", "/home/yuval/GoogleDrive/", "Beat_Saber_Dataset/")
