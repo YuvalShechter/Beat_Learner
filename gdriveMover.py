de5a8869d668ef13a00b47d35302d70029b21bf7
yuval <yshechter@umass.edu>
2020-04-20 13:35:24 -0400

    Made gdrivemove file better

--- gdriveMover.py
+++ gdriveMover.py
@@ -3,15 +3,15 @@ import subprocess
 import time
 import shutil
 
-def gdriveMove(folderpath, movepath):
-    for folder in os.listdir(folderpath):
-        shutil.move(folderpath+folder, movepath+folder)
+def gdriveMove(folderpath, mountpath, movepath):
+    if(os.path.ismount(mountpath)):
+        for folder in os.listdir(folderpath):
+            shutil.move(folderpath+folder, mountpath+movepath+folder)
+    else:
+        subprocess.run(["fusermount","-u",mountpath])
+        time.sleep(5)
+        subprocess.run(["google-drive-ocamlfuse",mountpath])
+        time.sleep(5)
 
 if __name__ == "__main__":
-    try:
-        gdriveMove("All Songs/", "/home/yuval/GoogleDrive/")
-    except:
-        subprocess.run(["fusermount","-u","/home/yuval/GoogleDrive"])
-        time.sleep(5)
-        subprocess.run(["google-drive-ocamlfuse","/home/yuval/GoogleDrive"])
-        time.sleep(5)
+        gdriveMove("All Songs/", "/home/yuval/GoogleDrive/", "Beat_Saber_Dataset/")
